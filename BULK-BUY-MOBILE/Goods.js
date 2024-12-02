b87898b35bd2db5d30972083bab21a9c21ae23d4
Oluwatosin1412 <133040429+Oluwatosin1412@users.noreply.github.com>
2024-12-02 13:07:50 +0100

    Add files via upload

---
+++ BULK-BUY-MOBILE/Goods.js
@@ -0,0 +1,53 @@
+import Rice from './images/Rice.png';
+import Oil from './images/download.jpeg'
+import Noodles from './images/Garri.jpeg'
+import Garri from './images/Garri Ijebu.jpeg'
+import Palmoil from './images/PalmOil.jpeg'
+import Beans from './images/Beans.jpeg'
+
+const goods = [
+  {
+    id:1,
+    image: Rice,
+    alt:"Image of Rice",
+    name:"Bag of Rice",
+    price:"₦90,000.00"
+  },
+  {
+    id:2,
+    image: Oil,
+    alt:"Image of Oil",
+    name:"Gallon of Oil",
+    price:"#45,000"
+  },
+  {
+    id: 3,
+    image: Garri,
+    alt: "Image of Garri",
+    name: "Bag of Garri",
+    price: "#25000"
+  },
+  {
+    id: 4,
+    image: Beans,
+    alt: "Image of Beans",
+    name: "Bag of Beans",
+    price: "#90000"
+  },
+  {
+    id: 5,
+    image: Noodles,
+    alt: "Image of Noodles",
+    name: "Carton of Noodles",
+    price: "#90000"
+  },
+  {
+    id: 6,
+    image: Palmoil,
+    alt: "Image of Oil",
+    name: "25ltrs of Palm Oil",
+    price: "#90000"
+  }
+  ];
+  
+  export default goods
